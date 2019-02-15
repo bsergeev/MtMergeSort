65de25123811f2dc30bb9a13940cc41eb9c5005c
Boris Sergeev <bsergeev@gmail.com>
2019-02-14 22:01:06 -0700

    Minor fixes

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Exploration of WebAssembly multi-threading on plain merge sort
 To build a native executable, run `build.sh` (on Windows, run it in _Git Bash_).  
 To build WebAssembly, compile with Emscripten (I used version 1.38.16):  
 ```
-emcc -std=c++17 -O3 -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=7 -s PROXY_TO_PTHREAD=1 -s TOTAL_MEMORY=1073741824 -o t.js main.cpp
+emcc -std=c++17 -O3 -DNDEBUG -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=7 -s PROXY_TO_PTHREAD=1 -s TOTAL_MEMORY=1073741824 -o t.js main.cpp
 ```
 Then create `index.html`, e.g.  
 ```html
@@ -21,7 +21,7 @@ and serve it with some HTTP server, e.g. `http-server` that can be installed wit
 ```
 npm i -g http-server
 ```
-Then open `localhost:8080` in Chrome with enabled webAssembly thread support, as described here:  
+Then open `localhost:8080` in Chrome with enabled WebAssembly thread support, as described here:  
 https://developers.google.com/web/updates/2018/10/wasm-threads  
 and look in the Console (F12).
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ public:
     void start() { _start = _end = ClockT::now(); }
     DurationT stop() { _end = ClockT::now();  return elapsed(); }
     DurationT elapsed() {
-        auto delta = std::chrono::duration_cast<TimeT>(_end - _start);
+        const auto delta = std::chrono::duration_cast<TimeT>(_end - _start);
         return static_cast<DurationT>(delta.count());
     }
 private:
@@ -25,13 +25,9 @@ private:
 };
 } // namespace util
 
-
-static const size_t N =
-#ifdef NDEBUG
-10'000'000;
-#else
-7;
-#endif
+// To build single-threaded code, change SINGLE_THREADED to true:
+constexpr bool SINGLE_THREADED = false;
+static const size_t N = 10'000'000;
 using value_t = float;
 
 // Merges two sections of A[]: A[l..m] and A[m+1..r] 
@@ -76,13 +72,16 @@ void merge(std::array<value_t, N>& A, size_t l, size_t m, size_t r) {
 //  }
 //} 
 void mergeSort(std::array<value_t, N>& arr, size_t l0, size_t r0) {
+  assert(l0 < r0);
+
   struct MergeSortParams {
-    size_t l, r;
-    size_t m;
+    size_t l, r; // parameters
+    size_t m;    // local variable
     size_t location;
   };
   std::stack<MergeSortParams> stk;
-  stk.push({ l0, r0, 0, 1 });
+  stk.push({ l0, r0, 0, 1 }); // for location = 1, m is not needed
+
   while (!stk.empty()) {
     const auto& stkTop = stk.top();
     const size_t l = stkTop.l;
@@ -94,13 +93,13 @@ void mergeSort(std::array<value_t, N>& arr, size_t l0, size_t r0) {
     if (l < r) {
       switch (location) {
       case 1:
-        m = (l + r) / 2;
+        m = l + (r - l) / 2; // = (l + r) / 2;
         stk.push({ l, r, m, 2 });
-        stk.push({ l, m, m, 1 });
+        stk.push({ l, m, 0, 1 });
         break;
       case 2:
         stk.push({ l, r, m, 3 });
-        stk.push({ m+1, r, m, 1 });
+        stk.push({ m+1, r, 0, 1 });
         break;
       case 3:
         merge(arr, l, m, r);
@@ -112,66 +111,59 @@ void mergeSort(std::array<value_t, N>& arr, size_t l0, size_t r0) {
   
 int main() 
 { 
+  // Allocate the array on the heap (to avoid stack overflow)
   auto pArr = std::make_unique<std::array<value_t, N>>();
   std::array<value_t, N>& arr = *(pArr.get());
 
-#ifdef NDEBUG
+  // Random initialize the array
   for (size_t i = 0; i < N; ++i) {
     arr[i] = rand() / static_cast<value_t>(RAND_MAX);
   }
-#else
-  size_t i = 0;
-  for (auto a : std::array<value_t, N>{38, 27, 43, 3, 9, 82, 10}) {
-    arr[i++] = a;
-  }
-#endif
-  
-  static const size_t N_THREADS = 4;
-  static_assert(N > N_THREADS, "N is too small!");
-  std::array<size_t, N_THREADS + 1> n;
-  for (size_t i = 0; i < N_THREADS; ++i) {
-    n[i] = i * N / N_THREADS;
-  }
-  n[N_THREADS] = N;
-
-  std::array<std::future<void>, N_THREADS> futures;
   
   util::Stopwatch<> stopwatch;
-#if 0
-  // Single-threaded sort (about 37.3 seconds)
-  mergeSort(arr, 0, N - 1);
-#else
-  // Sort on 4 + 2 threads (about 10.6 second)
-  for (size_t i = 0; i < N_THREADS; ++i) {
-    futures[i] = std::async(std::launch::async, 
-      [&arr, l=n[i], r=n[i+1]-1] { mergeSort(arr, l, r); });
-  }
+  if constexpr (SINGLE_THREADED) {
+    // Single-threaded sort (about 37.3 seconds for 10M array)
+    mergeSort(arr, 0, N - 1);
+  } else {
+    // Sort on 4 + 2 threads (about 10.6 second for 10M array)
+    static const size_t N_THREADS = 4;
+    static_assert(N > N_THREADS, "N is too small!");
+    std::array<size_t, N_THREADS + 1> n;
+    for (size_t i = 0; i < N_THREADS; ++i) {
+      n[i] = i * N / N_THREADS;
+    }
+    n[N_THREADS] = N;
 
-  std::array<std::future<void>, N_THREADS/2> futures2;
-  for (size_t i = 0; i < N_THREADS/2; ++i) {
-    futures2[i] = std::async(std::launch::async,
-      [&arr, &futures, i2=2*i, l=n[2*i], m=n[2*i+1]-1, r=n[2*i+2]-1] {
-        futures[i2].get(); futures[i2 + 1].get();
-        merge(arr, l, m, r);
-    });
+    stopwatch.start();
+
+    std::array<std::future<void>, N_THREADS> futures;
+    for (size_t i = 0; i < N_THREADS; ++i) {
+      futures[i] = std::async(std::launch::async,
+        [&arr, l = n[i], r = n[i + 1] - 1]{ mergeSort(arr, l, r); });
+    }
+
+    std::array<std::future<void>, N_THREADS / 2> futures2;
+    for (size_t i = 0; i < N_THREADS / 2; ++i) {
+      futures2[i] = std::async(std::launch::async,
+        [&arr, &futures, i2 = 2 * i, l = n[2 * i], m = n[2 * i + 1] - 1, r = n[2 * i + 2] - 1]{
+          futures[i2].get(); futures[i2 + 1].get();
+          merge(arr, l, m, r);
+        });
+    }
+    futures2[0].get();  futures2[1].get();
+    merge(arr, n[0], n[2] - 1, n[4] - 1);
   }
-  futures2[0].get();  futures2[1].get();
-  merge(arr, n[0], n[2]-1, n[4]-1);
-#endif
   const auto elapsed = stopwatch.stop();
   
+  // Check that the array is indeed sorted
   const bool sorted = [&arr]() {
-    auto prev = arr[0];
-    for (size_t i = 1; i < N; ++i)
-      if (prev > arr[i])
+    for (size_t i = 1; i < N; ++i) {
+      if (arr[i - 1] > arr[i])
         return false;
+    }
     return true;
   }();
   std::cout << "Sorted " << (sorted ? "successfully" : "UNSUCCESSFULLY") 
             << " in " << elapsed / 1'000'000 << " seconds" << std::endl;
-
-#ifndef NDEBUG
-  for (auto a : arr) std::cout << a << std::endl;
-#endif
   return 0;
 }
