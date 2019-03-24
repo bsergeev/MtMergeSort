ec462ea00c0d2a6f50382df736440dfce4127077
Boris Sergeev <bsergeev@gmail.com>
2019-03-24 15:34:54 -0600

    Preallocate temporary array (in all languages)

--- README.md
+++ README.md
@@ -13,11 +13,11 @@ g++ src/main.cpp -std=c++17 -g -O0 -lpthread s -fsanitize=address
 
 To build WebAssembly, compile with Emscripten (I used version 1.38.16). For single-threaded version, build as:  
 ```
-emcc -std=c++17 -Os -DNDEBUG  --llvm-lto 1 -s FILESYSTEM=0 -s TOTAL_MEMORY=1073741824 -o t.js main.cpp
+emcc -std=c++17 -Os -DNDEBUG  --llvm-lto 1 -s NO_FILESYSTEM=1 -s TOTAL_MEMORY=1073741824 -o t.js main.cpp
 ```
 and for multi-threaded runs it is necessary to enable filesystem emulation (for some reason, it is required even without memory init file) and set threading related options, e.g. for 4 threads:  
 ```
- emcc -std=c++17 -Os --llvm-lto 1 -s FILESYSTEM=1 -s -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=7 -s PROXY_TO_PTHREAD=1 -DNDEBUG -s TOTAL_MEMORY=1073741824 --memory-init-file 0 -o t.js main.cpp
+ emcc -std=c++17 -Os --llvm-lto 1 -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=7 -s PROXY_TO_PTHREAD=1 -DNDEBUG -s TOTAL_MEMORY=1073741824 --memory-init-file 0 -o t.js main.cpp
 ```
 Then create `index.html`, e.g.  
 ```html
@@ -38,6 +38,9 @@ https://developers.google.com/web/updates/2018/10/wasm-threads
 and look in the Console (F12).
 
 ## Results
-Native code shows about 3x speed up when sorting 10,000,000 floats on 4 threads. When running 8 threads on a 4-core CPU, oversubscription constitutes itself in slight slowdown. WebAssembly running in Chrome 72 shows the expected performance with a single thread (1.5...2 times as slow as the native code), but gets slower executing multi-threaded WebAssembly:  
-![Comparison](/doc/results.png)
-Sorting on a single thread takes native code 1.80 seconds, WebAssembly 3.1...3.3 seconds, manually written JavaScript 4.69 seconds, and Kotlin/JVM (Kotlin 1.3.21 with JRE 1.8) 2.2 seconds. Splitting the work between several threads causes the sort time in WebAssembly to inscrease, this tendency is worse in Chrome than in Firefox.
+Native code shows about 3x speed up when sorting 10,000,000 floats on 4 threads. When running 8 threads on a 4-core CPU, oversubscription constitutes itself in slight slowdown. WebAssembly running in Chrome 72 shows the expected performance with a single thread, about 1.5...2 times as slow as the native code. But, if left and right temporary arrays are allocated on the stack as `std::vector<value_t>` in each call to `merge()`, multi-threaded WebAssembly performance suffers greatly: 
+![Comparison 1](/doc/results_alloc.png)
+From the ather hand, pre-allocating temporary array ahead of time results in WebAssembly performance scaling similar to that of the native code:  
+![Comparison 2](/doc/results.png)
+Sorting on a single thread takes native code 1.55 seconds, WebAssembly 2.4...3.2 seconds, manually written JavaScript 2.62 seconds, and Kotlin/JVM (Kotlin 1.3.21 with JRE 1.8) 1.9 seconds:
+![Single thread](/doc/single_thread.png)
--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,20 @@
 
 // To build single-threaded code, change SINGLE_THREADED to true:
 constexpr bool SINGLE_THREADED = false;
+
 static const size_t N = 10'000'000; // size of the array to sort
 using value_t = float;
+using array = std::array<value_t, N>;
 
 // The following are only for multi-threaded code
 //#define FORCE_ASYNC
 #undef FORCE_ASYNC
-constexpr size_t N_LEVELS = 3; // 1 level -> 2 threads, 2 -> 4 threads, 3 -> 8 threads
+#ifdef FORCE_ASYNC
+#define ASYNC std::launch::async,
+#else
+#define ASYNC
+#endif
+constexpr size_t N_LEVELS = 2; // 1 level -> 2 threads, 2 -> 4 threads, 3 -> 8 threads
 constexpr size_t MAX_THREADS = 1 << N_LEVELS;
 static_assert(N > MAX_THREADS, "N is too small!");
 
@@ -44,55 +51,53 @@ private:
 };
 } // namespace util
 
-// Merges two sections of A[]: A[l..m] and A[m+1..r] 
-void merge(std::array<value_t, N>& A, size_t l, size_t m, size_t r) { 
+// Merges two sections of arr[]: arr[l..m] and arr[m+1..r] 
+void merge(array& arr, array& tmp, size_t l, size_t m, size_t r) {
   assert(l <= m && m <= r);
-  const size_t n1 = m - l + 1;
-  const size_t n2 = r - m;
 
-  // Copy data to left and right temp arrays, AL[] and AR[]
-  std::vector<value_t> AL(n1), AR(n2);
+  const size_t n1 = m - l + 1; // on the left
+  const size_t n2 = r - m;     // on the right
 
-  // Replacing loops with memcpy makes no discernible difference (optimizing
-  // compilers must be doing this).
+  // Copy data to left and right temp arrays (replacing loops with memcpy makes
+  // no discernible difference--optimizing compilers must be doing this).
   for (size_t i = 0; i < n1; ++i) {
-    AL[i] = A[l + i];
+    tmp[l + i] = arr[l + i];
   }
   for (size_t i = 0; i < n2; ++i) {
-    AR[i] = A[m + 1 + i];
+    tmp[m + 1 + i] = arr[m + 1 + i];
   }
   
-  // Merge the temp arrays back into A[l..r]
+  // Merge the temp arrays back into arr[l..r]
   size_t i = 0, j = 0, k = l;
   while (i < n1 && j < n2) {
-    A[k++] = (AL[i] <= AR[j])? AL[i++] : AR[j++];
+    arr[k++] = (tmp[l + i] <= tmp[m + 1 + j])? tmp[l + i++] : tmp[m + 1 + j++];
   }
 
 #if 0
-  // Copy the remaining elements of AL[], if any
+  // Copy the remaining elements of the left array, if any
   while (i < n1) {
-    A[k++] = AL[i++];
+    arr[k++] = tmp[l + i++];
   }
-  // Copy the remaining elements of AR[], if any
+  // Copy the remaining elements of the right array, if any
   while (j < n2) {
-    A[k++] = AR[j++];
+    arr[k++] = tmp[m + 1 + j++];
   }
 #else
   if (i < n1) {
-    std::memcpy(&A[k], &AL[i], (n1-i)*sizeof(value_t));
+    std::memcpy(&arr[k], &tmp[l + i], (n1-i)*sizeof(value_t));
   }
   if (j < n2) {
-    std::memcpy(&A[k], &AR[j], (n2-j)*sizeof(value_t));
+    std::memcpy(&arr[k], &tmp[m + 1 + j], (n2-j)*sizeof(value_t));
   }
 #endif
 }
 
-void mergeSort(std::array<value_t, N>& arr, size_t l, size_t r) {
+void mergeSort(array& arr, array& tmp, size_t l, size_t r) {
   if (l < r) {
     const size_t m = l + (r - l) / 2; // = (l + r)/2
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    merge(arr, l, m, r);
+    mergeSort(arr, tmp, l, m);
+    mergeSort(arr, tmp, m + 1, r);
+    merge(arr, tmp, l, m, r);
   }
 } 
 
@@ -100,21 +105,25 @@ int main()
 { 
   std::cout << "Sorting "<< N <<" floats on "<< (SINGLE_THREADED? "single thread" : std::to_string(MAX_THREADS)+" threads") << std::endl;
 
-  // Allocate the array on the heap (to avoid stack overflow)
+  // Allocate arrays on the heap (to avoid stack overflow)
   auto pArr = std::make_unique<std::array<value_t, N>>();
+  auto pTmp = std::make_unique<std::array<value_t, N>>();
+
   auto& arr = *(pArr.get());
+  auto& tmp = *(pTmp.get());
 
   // Random initialize the array
   for (size_t i = 0; i < N; ++i) {
     arr[i] = rand() / static_cast<value_t>(RAND_MAX);
   }
-  
+  tmp.fill(value_t{}); // not needed, really
+
   util::Stopwatch<> stopwatch;
 
-  if constexpr (SINGLE_THREADED) {
-    mergeSort(arr, 0, N - 1);
+  if constexpr (SINGLE_THREADED) { // ==========================================
+    mergeSort(arr, tmp, 0, N - 1);
   } 
-  else // multiple threads
+  else // multiple threads =====================================================
   {
     // Build indices of sub-arrays for the highest level. 
     // For lower levels, it'll be traversed with `stride` depending on the level.
@@ -123,63 +132,37 @@ int main()
     static std::array<std::future<void>, MAX_THREADS> sort_future;
     if constexpr (N_LEVELS == 1) //---------------------------------------------
     {
-      sort_future[0] = std::async([&arr] { mergeSort(arr, n[0], n[1]-1); });
-      sort_future[1] = std::async([&arr] { mergeSort(arr, n[1], n[2]-1); });
+      sort_future[0] = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[0], n[1]-1); });
+      sort_future[1] = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[1], n[2]-1); });
     }
     else if constexpr (N_LEVELS == 2) //----------------------------------------
     {
       // Sort four sub-arrays
-      static auto sort_future0 = std::async([&arr] { 
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        mergeSort(arr, n[0], n[1]-1); });
-      static auto sort_future1 = std::async([&arr] { 
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        mergeSort(arr, n[1], n[2]-1); });
-      static auto sort_future2 = std::async([&arr] { 
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        mergeSort(arr, n[2], n[3]-1); });
-      static auto sort_future3 = std::async([&arr] { 
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        mergeSort(arr, n[3], n[4]-1); });
+      static auto sort_future0 = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[0], n[1]-1); });
+      static auto sort_future1 = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[1], n[2]-1); });
+      static auto sort_future2 = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[2], n[3]-1); });
+      static auto sort_future3 = std::async( ASYNC [&arr, &tmp] { mergeSort(arr, tmp, n[3], n[4]-1); });
 
       // Merge two sub-arrays
-      sort_future[0] = std::async(
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        [&arr] { sort_future0.get(); sort_future1.get(); merge(arr, n[0], n[1]-1, n[2]-1); 
+      sort_future[0] = std::async( ASYNC [&arr, &tmp] { 
+        sort_future0.get(); sort_future1.get(); merge(arr, tmp, n[0], n[1]-1, n[2]-1); 
       });
-      sort_future[1] = std::async(
-        #ifdef FORCE_ASYNC
-        std::launch::async,
-        #endif
-        [&arr] { sort_future2.get(); sort_future3.get(); merge(arr, n[2], n[3]-1, n[4]-1);
+      sort_future[1] = std::async( ASYNC [&arr, &tmp] { 
+        sort_future2.get(); sort_future3.get(); merge(arr, tmp, n[2], n[3]-1, n[4]-1); 
       });
     }
     else if constexpr (N_LEVELS > 2) // 8 or more threads ----------------------
     {
       // Sort sub-arrays
       for (size_t i = 0; i < MAX_THREADS; ++i) {
-        sort_future[i] = std::async(
-          #ifdef FORCE_ASYNC
-          std::launch::async,
-          #endif
-          [&arr, i] { mergeSort(arr, n[i], n[i + 1]-1); });
+        sort_future[i] = std::async( ASYNC [&arr, &tmp, i] { mergeSort(arr, tmp, n[i], n[i + 1]-1); });
       }
 
       // Process each level by spawning worker threads to merge sub-arrays
       for (size_t level = N_LEVELS; level > 0; --level) {
         constexpr size_t one = 1;
-        const size_t N_threads = one << level;           // # of threads for this level
-        const size_t stride = one << (N_LEVELS - level); // stride for indices in `n` 
+        const size_t N_threads = one << level;              // # of threads for this level
+        const size_t stride    = one << (N_LEVELS - level); // stride for indices in `n` 
   
         // On 1st pass, wait for mergeSort() futures; later--on merge() futures from previous pass
         const size_t N_to_wait = (level == N_LEVELS)? MAX_THREADS : 2 * N_threads;
@@ -189,30 +172,26 @@ int main()
 
         // Merge sub-arrays
         for (size_t i = 0; i < N_threads; ++i) {
-          sort_future[i] = std::async(
-            #ifdef FORCE_ASYNC
-            std::launch::async,
-            #endif
-            [&arr, i, stride] { // stride is a power of 2 => stride/2 below is integer
-              merge(arr, n[stride*i], n[stride*i + stride/2]-1, n[stride*(i+1)]-1);
+          sort_future[i] = std::async( ASYNC
+            [&arr, &tmp, i, stride] { // stride is a power of 2 => stride/2 below is integer
+              merge(arr, tmp, n[stride*i], n[stride*i + stride/2]-1, n[stride*(i+1)]-1);
             });
         }
       } // level loop
     } //------------------------------------------------------------------------
     sort_future[0].get();  
     sort_future[1].get();
-    merge(arr, n[0], n[MAX_THREADS/2] - 1, n[MAX_THREADS] - 1);
-  }
+    merge(arr, tmp, n[0], n[MAX_THREADS/2] - 1, n[MAX_THREADS] - 1);
+  } // =========================================================================
 
   const auto elapsed_uS = stopwatch.stop();
   
   // Check that the array is indeed sorted
-  const bool sorted = [&arr]() {
-    for (size_t i = 1; i < N; ++i) 
-      if (arr[i - 1] > arr[i]) return false;
+  const bool ok = [&arr]() {
+    for (size_t i = 1; i < N; ++i) if (arr[i - 1] > arr[i]) return false;
     return true;
   }();
-  std::cout << "Sorted " << (sorted ? "successfully" : "UNSUCCESSFULLY") 
+  std::cout << "Sorted " << (ok ? "successfully" : "UNSUCCESSFULLY") 
             << " in " << elapsed_uS / 1'000'000 << " seconds" << std::endl;
-  return (sorted)? 0 : 13;
+  return (ok)? 0 : 13;
 }
